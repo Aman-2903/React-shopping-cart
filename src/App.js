690c464d6b334ac056e27ffe2251bb37be0ce5d6
Aman-2903 <kushwahaaman075@gmail.com>
2024-06-20 10:49:40 +0530

    add ProductsPage

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Navigation from "./component/Navigation";
-import Products from "./pages/Products";
+import ProductsPage from "./pages/ProductsPage";
 import Cart from "./pages/Cart";
+import SingleProduct from "./pages/SingleProduct";
 
 const App = () => {
   return (
@@ -14,7 +15,8 @@ const App = () => {
         <Routes>
           <Route path="/" Component={Home}></Route>
           <Route path="/about" Component={About}></Route>
-          <Route path="/products" Component={Products}></Route>
+          <Route path="/products" Component={ProductsPage}></Route>
+          <Route path="/products/:_id" Component={SingleProduct}></Route>
           <Route path="/cart" Component={Cart}></Route>
         </Routes>
       </Router>
--- src/pages/Products.js
+++
@@ -1,9 +0,0 @@
-const Products = () => {
-  return (
-    <>
-      <h1>This is product page</h1>
-    </>
-  );
-};
-
-export default Products;
