5c719aad418afcd776d2641c3163e3289dd3b5c9
Aman-2903 <kushwahaaman075@gmail.com>
2024-06-20 10:02:50 +0530

    add HomePage

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 //this is root component
-import {Route, BrowserRouter as Router,Routes} from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Navigation from './component/Navigation'
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Navigation from "./component/Navigation";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
 
 const App = () => {
   return (
     <>
-    <Router>
+      <Router>
         <Navigation></Navigation>
         <Routes>
-            <Route path='/' Component={Home}></Route>
-            <Route path='/about' Component={About}></Route>
+          <Route path="/" Component={Home}></Route>
+          <Route path="/about" Component={About}></Route>
+          <Route path="/products" Component={Products}></Route>
+          <Route path="/cart" Component={Cart}></Route>
         </Routes>
-    </Router>
+      </Router>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
